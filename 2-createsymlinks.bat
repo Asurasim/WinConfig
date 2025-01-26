eec078bea05cfc2630ff30677863d0853794adba
Asurasim <unknownstestmail@gmail.com>
2025-01-26 11:15:19 +0000

    Added wezterm to symlink script since it was forgotten

--- 2-createsymlinks.bat
+++ 2-createsymlinks.bat
@@ -1,4 +1,5 @@
 mklink /J %UserProfile%\.glzr glzr
+mklink /H %UserProfile%\.wezterm.lua wezterm.lua
 mklink /J %UserProfile%\AppData\Local\nvim nvim
 mkdir %UserProfile%\AppData\Roaming\yazi
 mklink /J %UserProfile%\AppData\Roaming\yazi\config yazi
